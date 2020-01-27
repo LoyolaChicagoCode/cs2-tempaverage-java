8a8595a3ed7a6d25a8add99083effd985e06897d
Konstantin Läufer <laufer@cs.luc.edu>
2020-01-27 14:18:47 -0600

    Update Average.java

--- src/Average.java
+++ src/Average.java
@@ -4,16 +4,16 @@ public class Average {
 
   public static double average(final double[] temps) {
     double sum = 0;
-    for (int i = 0; i < temps.length; i++) {    // read/store each day's temperature
-      sum += temps[i];
+    for (final double current: temps) {    // read/store each day's temperature
+      sum += current;
     }
     return sum / temps.length;
   }
 
   public static int daysAbove(final double[] temps, final double value) {
     int count = 0;                      // see if each day is above average
-    for (int i = 0; i < temps.length; i++) {
-      if (temps[i] > value) {
+    for (final double current: temps) {
+      if (current > value) {
         count++;
       }
     }
