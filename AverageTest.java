7b861e3100e70bf0926182967475cfd92c287559
Konstantin Läufer <laufer@cs.luc.edu>
2023-02-06 17:50:40 +0000

    Converted to simple Replit project layout

--- AverageTest.java
+++ AverageTest.java
@@ -1,14 +1,18 @@
-import static org.junit.Assert.*;
-
 import java.util.Collections;
 import java.util.List;
-import org.junit.Test;
 
 public class AverageTest {
 
   static final double DELTA = 0.000000001;
 
-  @Test
+  static void assertEquals(final int expected, final int actual) {
+    assert expected == actual;
+  }
+  
+  static void assertEquals(final double expected, final double actual) {
+    assert Math.abs(expected - actual) <= DELTA;
+  }
+  
   public void testAverage0() {
     final var temps = Collections.<Double>emptyList();
     final var actual = Average.calculateStats(temps);
@@ -16,18 +20,19 @@ public class AverageTest {
   }
 
   private void testAverage(
-      final List<Double> temps, final double expectedAverage, final int expectedCountAboveAverage) {
+    final List<Double> temps, 
+    final double expectedAverage, 
+    final int expectedCountAboveAverage
+  ) {
     final var actual = Average.calculateStats(temps);
     assertEquals(expectedAverage, actual.average, DELTA);
     assertEquals(expectedCountAboveAverage, actual.countAboveAverage);
   }
 
-  @Test
   public void testAverage1() {
     testAverage(List.of(3.4), 3.4, 0);
   }
 
-  @Test
   public void testAverage2() {
     testAverage(List.of(3.4, 3.6), 3.5, 1);
   }
